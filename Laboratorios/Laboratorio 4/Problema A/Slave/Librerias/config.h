a8822a0c44cb055db32026a5187ab32a143935ea
Sofía Modernell <sofia.modernell@estudiantes.utec.edu.uy>
2025-11-11 23:03:27 -0300

    Update config.h

--- Laboratorios/Laboratorio 4/Problema A/Slave/Librerias/config.h
+++ Laboratorios/Laboratorio 4/Problema A/Slave/Librerias/config.h
@@ -2,20 +2,19 @@
 #define CONFIG_H_
 
 /**
- * @brief Defina la frecuencia del reloj del sistema.
- * Es crucial para <util/delay.h> y los c·lculos de UART.
+ * Define la frecuencia del reloj del sistema.
+ * Es crucial para <util/delay.h> y los c√°lculos de UART.
  */
 #define F_CPU 16000000UL
 
 /**
- * @brief Interruptor de modo de compilaciÛn.
- * Defina esto como 1 para compilar para la simulaciÛn de Proteus
- * (usar· ADC/potenciÛmetros en lugar de DHT22).
+ * Interruptor de modo de compilaci√≥n.
+ * Definir esto como 1 para compilar para la simulaci√≥n de Proteus
+ * (usar√° ADC/potenci√≥metros en lugar de DHT22).
  *
- * Defina esto como 0 para compilar para el hardware fÌsico real
- * (usar· la biblioteca DHT22).
+ * Definir esto como 0 para compilar para el hardware f√≠sico real
+ * (usar√° la biblioteca DHT22).
  */
 #define SIMULATION_MODE 1
-
 
 #endif /* CONFIG_H_ */
