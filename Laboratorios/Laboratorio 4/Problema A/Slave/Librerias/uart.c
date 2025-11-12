bee7dceed1cace601aa8129debe997d30d1cb644
Sofía Modernell <sofia.modernell@estudiantes.utec.edu.uy>
2025-11-11 23:04:54 -0300

    Update uart.c

--- Laboratorios/Laboratorio 4/Problema A/Slave/Librerias/uart.c
+++ Laboratorios/Laboratorio 4/Problema A/Slave/Librerias/uart.c
@@ -1,30 +1,24 @@
-#include "config.h" // AÑADIDO: Para F_CPU
+#include "config.h" //Para F_CPU
 #include <avr/io.h>
 #include <util/delay.h>
-#include <stdlib.h> // <-- *** ARREGLO CRÍTICO: Añadido para itoa() ***
+#include <stdlib.h> // AÃ±adido para itoa() 
 #include "uart.h"
 
 void uart_init(uint32_t baud){
-	// Calculate UBRR value
+
 	uint16_t ubrr = (F_CPU / 16 / baud) - 1;
 	
-	// Set baud rate
 	UBRR0H = (uint8_t)(ubrr >> 8);
 	UBRR0L = (uint8_t)(ubrr);
 	
-	// Enable Transmitter (TX)
-	// Add (1<<RXEN0) if you need to receive data
 	UCSR0B = (1 << TXEN0);
 	
-	// Set frame format: 8data, 1stop bit (8N1)
 	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
 void uart_tx(uint8_t c){
-	// Wait for empty transmit buffer
 	while (!(UCSR0A & (1 << UDRE0)));
 	
-	// Put data into buffer, sends the data
 	UDR0 = c;
 }
 
@@ -36,6 +30,7 @@ void uart_print(const char *s){
 
 void uart_print_num(int num){
 	char buf[12];
-	itoa(num, buf, 10); // Esta función requiere stdlib.h
+	itoa(num, buf, 10); 
 	uart_print(buf);
+
 }
