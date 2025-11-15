b27bad3c33168836a574b077cd24548cab1ee729
Sofía Modernell <sofia.modernell@estudiantes.utec.edu.uy>
2025-11-14 21:46:36 -0300

    Update MQ135.c

--- Laboratorios/Laboratorio 4/Problema B /Librerias/MQ135.c
+++ Laboratorios/Laboratorio 4/Problema B /Librerias/MQ135.c
@@ -1,4 +1,4 @@
-#include "config.h" // AÑADIDO: Para F_CPU
+#include "config.h" 
 #include <avr/io.h>
 #include "MQ135.h"
 
@@ -6,18 +6,13 @@ static uint8_t mq135_ch = 0;
 
 void mq135_init(uint8_t adc_channel){
 	mq135_ch = adc_channel & 0x0F;
-	
-	// NOTA: La inicialización del ADC (ADMUX, ADCSRA)
-	// se movió a la función ADC_Init() en main.c,
-	// ya que el ADC es compartido por 3 sensores.
-	// Solo guardamos el canal aquí.
 }
 
 uint16_t mq135_read_raw(void){
 	// Seleccionar el canal del MQ135
 	ADMUX = (ADMUX & 0xF0) | (mq135_ch & 0x0F);
 	
-	// Iniciar conversión
+	// Iniciar conversiÃ³n
 	ADCSRA |= (1 << ADSC);
 	
 	// Esperar a que termine
@@ -25,3 +20,4 @@ uint16_t mq135_read_raw(void){
 	
 	return (uint16_t)ADC;
 }
+
